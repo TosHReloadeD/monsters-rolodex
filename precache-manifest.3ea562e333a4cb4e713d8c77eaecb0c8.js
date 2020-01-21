2cd4e0d810014c40de16aeb2497ab50e421a8827
Piero Giovanni <pierogiovanni93@gmail.com>
2020-01-21 13:58:19 -0500

    Updates

---
+++ precache-manifest.3ea562e333a4cb4e713d8c77eaecb0c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7da8fc8c1cd60e590f324e3bf7c7f3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "794f30cd4c6a6f4a69e9",
+    "url": "/monsters-rolodex/static/css/main.b926d4d4.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "794f30cd4c6a6f4a69e9",
+    "url": "/monsters-rolodex/static/js/main.22643bf4.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
